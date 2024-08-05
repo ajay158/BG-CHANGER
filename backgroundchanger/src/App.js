fcabb4c701f61306f0830e3e6682196d4af51fab
Ajay Kumar Shahu <1905922@kiit.ac.in>
2024-08-05 14:54:23 +0545

    bgchanger

---
+++ backgroundchanger/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
